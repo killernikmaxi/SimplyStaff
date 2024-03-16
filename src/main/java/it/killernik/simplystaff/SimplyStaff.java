34809b0c90cb4cbf30243654254dde9157825e1d
killernik <103605100+killernik@users.noreply.github.com>
2024-03-16 20:44:31 +0100

    First commit

---
+++ src/main/java/it/killernik/simplystaff/SimplyStaff.java
@@ -0,0 +1,17 @@
+package it.killernik.simplystaff;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimplyStaff extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
