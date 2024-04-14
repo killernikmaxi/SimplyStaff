1510a40f03bf27552e6c8169b5c6c78ddf1507a4
killernikmaxi <103605100+killernik@users.noreply.github.com>
2024-04-14 18:38:16 +0200

    Added: /endersee

---
+++ src/main/java/it/killernik/simplystaff/Commands/Basic/EnderSeeCommand.java
@@ -0,0 +1,54 @@
+package it.killernik.simplystaff.Commands.Basic;
+
+import it.killernik.simplystaff.SimplyStaff;
+import it.killernik.simplystaff.Utils.MessageUtil;
+import it.killernik.simplystaff.Utils.StaffAlert;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class EnderSeeCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+
+        if (!(commandSender instanceof Player)) {
+
+            commandSender.sendMessage(MessageUtil.message(SimplyStaff.INSTANCE.getConfig().getString("ERROR.only-player"), null));
+            return true;
+        }
+
+        if (!commandSender.hasPermission("ss.endersee")) {
+            commandSender.sendMessage(MessageUtil.message(SimplyStaff.INSTANCE.getConfig().getString("ERROR.no-permission"), null));
+            return true;
+        }
+
+        if (args.length == 0) {
+
+            commandSender.sendMessage(MessageUtil.message(SimplyStaff.INSTANCE.getConfig().getString("ERROR.specify-player"), null));
+            return true;
+
+        } else if (args.length == 1) {
+
+            String pname = args[0];
+            Player player = Bukkit.getPlayer(pname);
+
+            if (player == null) {
+                commandSender.sendMessage(MessageUtil.message(SimplyStaff.INSTANCE.getConfig().getString("ERROR.non-existent-player"), null));
+                return true;
+            } else {
+
+                commandSender.sendMessage(MessageUtil.message(SimplyStaff.INSTANCE.getConfig().getString("COMMANDS.Basic.endersee"), player));
+                ((Player) commandSender).openInventory(player.getEnderChest());
+
+                return true;
+            }
+
+        } else {
+            commandSender.sendMessage(MessageUtil.message(SimplyStaff.INSTANCE.getConfig().getString("ERROR.incorrect-syntax"), null));
+            return true;
+        }
+    }
+
+}
